51a1ec6e7288954d002003d8d85ac1a7adefae62
Amin Eyvazlou <cs.svftf@gmail.com>
2022-04-07 05:55:06 +0430

    add cookies and headers

--- scrape/main.py
+++ scrape/main.py
@@ -2,6 +2,8 @@ from typing import List
 import requests
 import json
 
+from scrape.metadata import COOKIES, HEADERS
+
 
 # class Hashtag:
 #     def __init__(self, query):
@@ -11,7 +13,7 @@ import json
 #             f"https://instagram.com/explore/tags/{query}/?__a=1"
 #         ).json()
 #         print("AFTER GRAPHQL")
-#         self.hashtag_data = scrape["graphql"]["hashtag"]["edge_hashtag_to_media"]
+#         self.ag_data = scrape["graphql"]["hashtag"]["edge_hashtag_to_media"]
 #         self.count = self.hashtag_data["count"]
 #         self.data_container = self.hashtag_data["edges"]
 #         print("INIT FINISH")
@@ -51,23 +53,11 @@ def scrape_http(query):
 
     data = requests.get(
         f"https://instagram.com/explore/tags/{query}/?__a=1",
-        headers={
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Cookie": 'mid=YkSqfAAEAAGicyuRvG6XLSt8mqjx; ig_did=93DEC88A-4441-4C82-B215-82B374288912; ig_nrcb=1; shbid="4998\0549995949688\0541680626518:01f7de189be75bf44cc08cdcca6d367d64e504680c602578dcb6e637392d6e8a5690b019"; shbts="1649090518\0549995949688\0541680626518:01f7e47fcc3e286b74fb2a9f9022c635cf338ec6bbdabbca94314033ee81d4ac37f088a3"; csrftoken=PTWyyPvqMCYmLvWAmJCB45z5F9RbiDMR; ds_user_id=9995949688; sessionid=9995949688%3AwSUp9EmUQ2Yq6F%3A1; rur="LDC\0549995949688\0541680822623:01f70a615cf734a734e53da385592b2a5285ac774780725bf861e66abf43918ed22e4fd4"',
-            "Host": "www.instagram.com",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "cross-site",
-            "TE": "trailers",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (X11;Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0",
-        },
+        headers=HEADERS,
+        cookies=COOKIES,
     ).json()
     print(data.headers)
+    # TODO: return the data from scraped web page.
 
 
 def _save_to_file(dict_data) -> None:
